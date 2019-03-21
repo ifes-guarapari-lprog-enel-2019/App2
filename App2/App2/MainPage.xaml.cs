728fab9b0d400b69e7ca2518deb5ea39a657d514
walberbeltrame <walber.beltrame@gmail.com>
2019-03-21 15:34:50 -0300

    Versão estruturada.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            // desabilitar mensagens
+            Label1.IsVisible = false;
+            Label2.IsVisible = false;
+            // verificar se período e semestre foram selecionados
+            if (Entry2.Text != null &&
+                Entry2.Text.Length > 0 &&
+                Picker.SelectedIndex >= 0)
+            {
+                // criar variáveis 
+                int periodo = int.Parse(Entry2.Text);
+                int semestre = Picker.SelectedIndex + 1;
+                // verificar se os valores são iguais
+                if (periodo == semestre)
+                {
+                    // habilitar mensagem de erro
+                    Label1.IsVisible = true;
+                } else
+                {
+                    // habilitar mensagem de erro
+                    Label2.IsVisible = true;
+                }
+            } else
+            {
+                // habilitar mensagem de erro
+                Label2.IsVisible = true;
+            }
+
+        }
+
+    }
+}
